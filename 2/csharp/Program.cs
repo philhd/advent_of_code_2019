ed4ed3907cee91339d7d6c3ee2fc83ca1dc775a6
Phil Hardegen <philhardegen@gmail.com>
2019-12-03 17:58:44 -0500

    cleanup

--- 2/csharp/Program.cs
+++ 2/csharp/Program.cs
@@ -10,36 +10,37 @@ namespace csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             var lines = "../input.txt".ReadFromFile();
             var linesList = lines.ToList();
-
             var numbers = linesList.First().Split(',').Select(x => int.Parse(x)).ToList();
-            // Console.WriteLine(numbers.AsJSON());
 
             // TEST
-            var testInput = "1,9,10,3,2,3,11,0,99,30,40,50";
-            var testNumbers = testInput.Split(',').Select(x => int.Parse(x)).ToList();
+            // var testInput = "1,9,10,3,2,3,11,0,99,30,40,50";
+            // var testNumbers = testInput.Split(',').Select(x => int.Parse(x)).ToList();
             Run(numbers);
             Console.WriteLine(numbers.AsJSON());
         }
 
         static void Run(List<int> numbers){
-            resetAlarm(numbers);
-            compute(numbers);
+            ResetAlarm(numbers);
+            Compute(numbers);
         }
 
         // Once you have a working computer, the first step is to restore the gravity 
         // assist program (your puzzle input) to the "1202 program alarm" state it had 
         // just before the last computer caught fire. To do this, before running the program,
         //  replace position 1 with the value 12 and replace position 2 with the value 2.
-        static void resetAlarm(List<int> numbers)
+        static void ResetAlarm(List<int> numbers)
         {
-            numbers[1] = 12;
-            numbers[2] = 2;
+            SetInputs(numbers, 12, 2);
         }
 
-        static void compute(List<int> numbers){
+        static void SetInputs(List<int> numbers, int noun, int verb){
+            numbers[1] = noun;
+            numbers[2] = verb;
+        }
+
+        static void Compute(List<int> numbers){
             for(int i = 0;i<numbers.Count; i+=4){
                 var opcode = numbers[i];
                 if(opcode == 99){
