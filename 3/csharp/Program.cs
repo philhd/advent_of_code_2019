5a62df63307024d8d7670473b7b9ef42606a2c63
Phil Hardegen <philhardegen@gmail.com>
2019-12-03 18:35:18 -0500

    3a initial commit

---
+++ 3/csharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using CoreExtensions.File;
+using CoreExtensions.Json;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = "../input.txt".ReadFromFile().ToList();
+            Console.WriteLine(input.AsJSON());
+        }
+    }
+}
