d51e10e001df8ed3189720b11bba70f54614a1b5
Phil Hardegen <philhardegen@gmail.com>
2019-12-06 19:23:39 -0500

    solution for 6b

--- 6/csharp/Program.cs
+++ 6/csharp/Program.cs
@@ -12,22 +12,26 @@ namespace csharp
         {
             var input = "../input.txt".ReadFromFile().ToList();
             // var input = "../testInput.txt".ReadFromFile().ToList();
-
+            // var input = "../testInput2.txt".ReadFromFile().ToList();
+            
             Console.WriteLine(input.AsJSON());
 
             // map from node to parent
-            var tree = new Dictionary<string,string>();
+            var tree = new Dictionary<string, string>();
 
-            foreach(var link in input){
+            foreach (var link in input)
+            {
                 var nodes = link.Split(')');
                 var child = nodes[1];
                 var parent = nodes[0];
 
                 // lazy initialize nodes to point to themself
-                if(!tree.ContainsKey(parent)){
+                if (!tree.ContainsKey(parent))
+                {
                     tree[parent] = parent;
                 }
-                if(!tree.ContainsKey(child)){
+                if (!tree.ContainsKey(child))
+                {
                     tree[child] = child;
                 }
 
@@ -38,10 +42,12 @@ namespace csharp
             var edgesCount = 0;
 
             // for each node, count the edges to the root
-            foreach(var node in tree.Keys){
+            foreach (var node in tree.Keys)
+            {
                 // Console.WriteLine($"for {node}");
                 var currentNode = node;
-                while(tree[currentNode] != currentNode){
+                while (tree[currentNode] != currentNode)
+                {
                     // Console.WriteLine($"adding edge for {currentNode}->{tree[currentNode]}");
                     edgesCount++;
                     currentNode = tree[currentNode];
@@ -49,6 +55,65 @@ namespace csharp
             }
 
             Console.WriteLine(edgesCount);
+
+            // Find paths to root
+            var path1 = GetPath(tree, "SAN", "COM");
+            var path2 = GetPath(tree, "YOU", "COM");
+            Console.WriteLine(path1.AsJSON());
+            Console.WriteLine(path2.AsJSON());
+
+            // Find common root
+            var commonRoot = GetCommonRoot(tree, path1, path2);
+
+            // Find distance from each to the common root
+            var dist1 = GetPath(tree, "SAN", commonRoot);
+            var dist2 = GetPath(tree, "YOU", commonRoot);
+
+            // subtract 1 for each because it's the orbitee not the orbiter
+            // subtract another to get the path length to get edge count from node count
+            Console.WriteLine(dist1.Count - 2 + dist2.Count - 2);
+        }
+
+        static string GetCommonRoot(Dictionary<string, string> tree, List<string> path1, List<string> path2)
+        {
+            // trace back from main root to find common root
+            path1.Reverse();
+            path2.Reverse();
+            for (int i = 0; i < Math.Min(path1.Count, path2.Count); i++)
+            {
+                if (path1[i] != path2[i])
+                {
+                    return path1[i - 1];
+                }
+            }
+            throw new InvalidOperationException("no common root found");
+        }
+
+        static List<string> GetPath(Dictionary<string, string> tree, string start, string end)
+        {
+            var nodes = new List<string>();
+            var currentNode = start;
+            while (true)
+            {
+                // Console.WriteLine($"adding edge for {currentNode}->{tree[currentNode]}");
+                nodes.Add(currentNode);
+
+                // if we reach the root before finding the end, there is no path
+                if (tree[currentNode] == currentNode)
+                {
+                    throw new InvalidOperationException("no path found");
+                }
+
+                // exit condition
+                if (tree[currentNode] == end)
+                {
+                    nodes.Add(tree[currentNode]);
+                    break;
+                }
+                currentNode = tree[currentNode];
+            }
+
+            return nodes;
         }
     }
 }
